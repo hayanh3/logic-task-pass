31c82c1b5b762528b568e4f944dbc67adbe978eb
hayanh3 <60624082+hayanh3@users.noreply.github.com>
2022-08-12 21:26:38 +0300

    Update Q3 how many the given character repeated.py

--- logic-pass/Q3 how many the given character repeated.py
+++ logic-pass/Q3 how many the given character repeated.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 def repeated_char(string,char):
     return f"{char} is repeated {Counter(string)[char]} times" if char in string else f'{char} is not in String'
-print(repeated_char('haya','k'))
+print(repeated_char(input('Enter your string: '),input('Enter character to check: ')))
